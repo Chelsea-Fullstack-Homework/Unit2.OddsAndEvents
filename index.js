42be3c8ffbe092a7643afa60bd23c793e5430395
Chelsea Scott <chelseacat62@gmail.com>
2023-10-27 05:41:41 -0700

    reading in input from value using button.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // TODO: this file! :)
+const searchForum = document.querySelector("form");
+const addButton = document.querySelector(".submit-btn");
+const inputNum = document.getElementById("number");
+
+searchForum.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log(inputNum.value);
+});
+
